12fcaed8f5e90a76f7d1e0739857365b4b445d1d
JHero <jheroytc@gmail.com>
2024-05-25 04:25:08 -0500

    firt commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <!-- <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -25,13 +25,16 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </ul> -->
+    <h1>JHero</h1>
+    <h3>Gamer | Coder | Actor</h3>
+    <img src="../assets/headshot2024.jpg" alt="">
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Home',
   props: {
     msg: String
   }
@@ -40,7 +43,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
+/* h3 {
   margin: 40px 0 0;
 }
 ul {
@@ -53,5 +56,11 @@ li {
 }
 a {
   color: #42b983;
+} */
+h1, h3 {
+  text-align: center;
+}
+img {
+  width: 75%;
 }
 </style>
